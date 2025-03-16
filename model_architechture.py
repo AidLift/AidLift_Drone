7254495d6b2e3dbd4d9dadc589ae04d3cf49a222
Thomas4534 <deleuzethomasbisson@gmail.com>
2025-03-16 02:25:00 -0400

    Create model_architechture.py

---
+++ model_architechture.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import torch.optim as optim
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(128 * 16 * 16, 512)  
+        self.fc2 = nn.Linear(512, 2)  
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 16 * 16) 
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+model = ConvNet()
