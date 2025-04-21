91a6a95526f11e812f5f7545ae78d8aff1b31dbd
DomCs <cuscunadomenico123@gmail.com>
2025-04-21 10:17:17 -0400

    Download the best_model.pth directly at runtime from google drive

--- Pathfinding/api.py
+++ Pathfinding/api.py
@@ -278,7 +278,9 @@ def save_grid_info():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
 
 @app.route('/get-hospitals', methods=['GET'])
 def get_hospitals():
