555354ae7e9bf668fefec1d77692866751510b8b
DomCs <cuscunadomenico123@gmail.com>
2025-04-22 09:14:14 -0400

    Change the dist management in express server

--- Pathfinding/download_model.py
+++ Pathfinding/download_model.py
@@ -13,7 +13,7 @@ file_id = '1kI0P8QFgpY6GJsSKKoA1V6wPcpC0Vi_t'  # Updated file ID
 url = f'https://drive.google.com/uc?export=download&id={file_id}'
 
 # Define the file path where the model will be saved
-model_file_path = os.path.join(models_folder, "best_model.pth")
+model_file_path = os.path.join(models_folder, "best_model_extended.pth")
 
 # Download the model file from Google Drive
 gdown.download(url, model_file_path, quiet=False)
--- website/server/api.js
+++ website/server/api.js
@@ -15,8 +15,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+
 
 
 app.post('/hospitals', (req, res) => {
